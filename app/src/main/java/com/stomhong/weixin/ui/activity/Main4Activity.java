aab4d9b688e54b67c55af4e2284819eafad8617d
stomhong <stomhong@163.com>
2016-11-25 16:49:30 +0800

    按功能模块划分包

--- app/src/main/java/com/stomhong/weixin/ui/activity/Main4Activity.java
+++ app/src/main/java/com/stomhong/weixin/ui/activity/Main4Activity.java
@@ -11,6 +11,5 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-
     }
 }
